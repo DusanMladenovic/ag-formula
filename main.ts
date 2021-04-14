d9f5f584297ffc7d95c7b225e71d236f1900a0cc
DusanMladenovic <77387201+DusanMladenovic@users.noreply.github.com>
2021-04-14 10:27:03 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,19 @@ namespace SpriteKind {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     SPEED = 10
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.RightSide, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprite.right = otherSprite.right - 0
+    scene.cameraShake(4, 200)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.LeftSide, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
+    sprite.left = otherSprite.left + 0
+    scene.cameraShake(4, 200)
 })
 let roadSlice2: Sprite = null
 let roadSlice: Sprite = null
+let turnOffset = 0
 let SPEED = 0
 let mySprite = sprites.create(img`
     . . . . . . 7 7 7 7 7 7 . . . . 
@@ -31,8 +39,9 @@ let mySprite = sprites.create(img`
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 SPEED = 0
-info.setLife(3)
+info.setLife(100)
 game.onUpdateInterval(100, function () {
+    turnOffset += randint(-2, 2)
     roadSlice = sprites.createProjectileFromSide(img`
         7777777777777777777777777777cccccc7777777777777777777777cccc....................
         77777777777777777777777777777cccccc777777777777777777777cccc....................
@@ -47,6 +56,7 @@ game.onUpdateInterval(100, function () {
         `, 0, 80)
     roadSlice.right = 50
     roadSlice.setKind(SpriteKind.RightSide)
+    roadSlice.x += 0 - turnOffset
     roadSlice2 = sprites.createProjectileFromSide(img`
         ........cccccc7888777777777777777777ccccc777777777777777777777777777777777777777
         ........ccccc7777777777778887777777ccccc7777777777777777777777777777777777777777
@@ -61,4 +71,5 @@ game.onUpdateInterval(100, function () {
         `, 0, 80)
     roadSlice2.right = 200
     roadSlice2.setKind(SpriteKind.LeftSide)
+    roadSlice2.x += 0 - turnOffset
 })
